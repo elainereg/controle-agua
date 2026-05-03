483f453c772e20a3919a9c55e119394013be19d6
Elaine Regina <268435265+elainereg@users.noreply.github.com>
2026-05-03 10:22:17 -0300

    Add files via upload
    primeiro commit

---
+++ controle_agua.py.py
@@ -0,0 +1,34 @@
+from colorama import Fore, Style, init
+
+# inicia o colorama
+init()
+
+# lista com os níveis e mensagens
+niveis = [
+    "Nível 1 - Muito baixo (crítico)",
+    "Nível 2 - Baixo",
+    "Nível 3 - Médio",
+    "Nível 4 - Alto",
+    "Nível 5 - Muito alto (alerta)"
+]
+
+# função que define a cor
+def definir_cor(nivel):
+    if nivel == 1:
+        return Fore.RED
+    elif nivel == 2:
+        return Fore.YELLOW
+    elif nivel == 3:
+        return Fore.GREEN
+    elif nivel == 4:
+        return Fore.CYAN
+    elif nivel == 5:
+        return Fore.BLUE
+
+# simulação dos níveis
+for i in range(5):
+    cor = definir_cor(i + 1)
+    print(cor + niveis[i])
+
+# volta ao padrão
+print(Style.RESET_ALL)
